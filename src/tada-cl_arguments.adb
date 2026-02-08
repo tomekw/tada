cc8da6c33ba481e21e5a7cc3ad4110b620f2d6e5
Tomek Walkuski <git@tomekw.com>
2026-02-08 22:47:25 +0100

    No need to init vector

--- src/tada-cl_arguments.adb
+++ src/tada-cl_arguments.adb
@@ -2,7 +2,7 @@ with Ada.Command_Line;
 
 package body Tada.CL_Arguments is
    function Consume return Argument_List.Vector is
-      Arguments : Argument_List.Vector := Argument_List.Empty_Vector;
+      Arguments : Argument_List.Vector;
    begin
       for I in 1 .. Ada.Command_Line.Argument_Count loop
          Arguments.Append (Ada.Command_Line.Argument (I));
