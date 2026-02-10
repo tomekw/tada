ed824bd9608ff15aa5508d2a91749fcdc0b23722
Tomek Walkuski <git@tomekw.com>
2026-02-10 10:23:00 +0100

    Scripts not needed anymore, tada is self-hosted now

--- scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-PROFILE="${1:-debug}"
-
-echo "Running (profile: $PROFILE)..."
-echo ""
-
-./target/$PROFILE/bin/tada
--- scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-PROFILE="${1:-debug}"
-
-echo "Running tests (profile: $PROFILE)..."
-
-./target/$PROFILE/bin/run_tests
