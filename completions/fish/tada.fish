49bc0dc095d7ff195db4ccdca4fed9e158f3263c
Tomek Walkuski <git@tomekw.com>
2026-04-17 16:38:47 +0200

    Update fish completions

--- completions/fish/tada.fish
+++ completions/fish/tada.fish
@@ -2,11 +2,9 @@ complete -c tada -f
 
 complete -c tada -n __fish_use_subcommand -a build -d "Compile the package"
 complete -c tada -n "__fish_seen_subcommand_from build" -l profile -rf -a "debug release" -d "Build profile"
-complete -c tada -n "__fish_seen_subcommand_from build" -s p -rf -a "debug release" -d "Build profile"
 
 complete -c tada -n __fish_use_subcommand -a cache -d "Install package to the local cache, use --force to overwrite"
 complete -c tada -n "__fish_seen_subcommand_from cache" -l force -d "Overwrite cache"
-complete -c tada -n "__fish_seen_subcommand_from cache" -s f -d "Overwrite cache"
 
 complete -c tada -n __fish_use_subcommand -a clean -d "Remove build artifacts"
 
@@ -16,18 +14,14 @@ complete -c tada -n __fish_use_subcommand -a help -d "Show help"
 
 complete -c tada -n __fish_use_subcommand -a init -d "Create a new package"
 complete -c tada -n "__fish_seen_subcommand_from init" -l name -rf -d "Package name"
-complete -c tada -n "__fish_seen_subcommand_from init" -s n -rf -d "Package name"
 complete -c tada -n "__fish_seen_subcommand_from init" -l type -rf -a "exe lib" -d "Package type"
-complete -c tada -n "__fish_seen_subcommand_from init" -s t -rf -a "exe lib" -d "Package type"
 
 complete -c tada -n __fish_use_subcommand -a install -d "Install dependencies"
 
 complete -c tada -n __fish_use_subcommand -a run -d "Build and run the executable"
 complete -c tada -n "__fish_seen_subcommand_from run" -l profile -rf -a "debug release" -d "Run profile"
-complete -c tada -n "__fish_seen_subcommand_from run" -s p -rf -a "debug release" -d "Run profile"
 
 complete -c tada -n __fish_use_subcommand -a test -d "Build and run the tests"
 complete -c tada -n "__fish_seen_subcommand_from test" -l profile -rf -a "debug release" -d "Test profile"
-complete -c tada -n "__fish_seen_subcommand_from test" -s p -rf -a "debug release" -d "Test profile"
 
 complete -c tada -n __fish_use_subcommand -a version -d "Show version"
